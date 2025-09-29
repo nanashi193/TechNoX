d52410c5f775642f2f5db2facdde1655b0201b2f
Nguyen Phuc Thinh <thinhnpse183789@fpt.edu.vn>
2025-09-29 22:44:46 +0700

    First commit

---
+++ shop/backend/src/main/java/com/g5/techdevices/techstore/configurations/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.g5.techdevices.techstore.configurations;
+
+/*import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(){
+    return ("haha");
+    }
+
+
+}*/
---
+++ shop/backend/src/main/java/com/g5/techdevices/techstore/dto/ProductDTO.java
@@ -0,0 +1,34 @@
+package com.g5.techdevices.techstore.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO {
+    private Long productId;// bigint
+
+    @JsonProperty("category_id")
+    private String categoryId;// int
+
+    @NotBlank(message = "Title is required!")
+    @Size (min = 3, max = 200, message = "Title must be between 3 and 200 characters")
+    private String name;            // nvarchar(255)
+
+    @Min(value = 0, message = "Price must be great than or equal to 0")
+    private Double price;       // decimal(18,2)
+
+    @JsonProperty("category_id")
+    private String description;     // nvarchar(MAX)
+
+    private String imageUrl;        // nvarchar(1000)
+    private LocalDateTime createdAt; // datetime2(7)
+}
