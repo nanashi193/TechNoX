3cdf10b178ba33ee16b216b33973f838d8ee03aa
nanashi193 <dqhungg8357@gmail.com>
2025-10-03 20:55:55 +0700

    modify homePage and name

--- shop/frontend/src/app/app.routes.ts
+++ shop/frontend/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'intro', pathMatch: 'full' },  // vào intro trước
+
+    { path: 'home',  component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
-    { path: '', pathMatch: 'full', redirectTo: 'login' },
-    { path: '**', redirectTo: 'login' }
+    { path: '**', redirectTo: 'home' },
+
 ];
--- shop/frontend/src/app/components/home/home.component.ts
+++ shop/frontend/src/app/components/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component, AfterViewInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
