4e7ebc6ee580271a7f2732256e93efc9d9e1faba
nanashi <dqhungg8357@gmail.com>
2025-10-05 04:31:46 +0000

    Revert "Merge branch 'develop-fe-test_home' into 'main'"
    This reverts merge request !9

--- shop/frontend/src/app/app.routes.ts
+++ shop/frontend/src/app/app.routes.ts
@@ -1,16 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { HomeComponent } from './components/home/home.component';
-import {ForgotPasswordComponent} from "./components/forgot-password/forgot-password.component";
+import { LoginComponent } from './features/auth/login';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'intro', pathMatch: 'full' },  // vào intro trước
-
-    { path: 'home',  component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: 'signup', component: SignupComponent },
-    {path: 'forgot-password', component: ForgotPasswordComponent},
-    { path: '**', redirectTo: 'home' },
-
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
