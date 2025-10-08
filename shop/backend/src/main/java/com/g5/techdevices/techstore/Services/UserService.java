af2b7ce70d3d179c83f5dd3e2c457a0855a2933e
thunder <trungdinh.dev@gmail.com>
2025-10-08 23:10:32 +0700

    Processing JwtToken for login

--- shop/backend/src/main/java/com/g5/techdevices/techstore/Services/UserService.java
+++ shop/backend/src/main/java/com/g5/techdevices/techstore/Services/UserService.java
@@ -1,5 +1,6 @@
 package com.g5.techdevices.techstore.Services;
 
+import com.g5.techdevices.techstore.components.JwtTokenUtil;
 import com.g5.techdevices.techstore.dto.UserDTO;
 import com.g5.techdevices.techstore.entity.users.Role;
 import com.g5.techdevices.techstore.entity.users.User;
@@ -8,15 +9,23 @@ import com.g5.techdevices.techstore.repositories.RoleRepository;
 import com.g5.techdevices.techstore.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Base64;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements IUserService{
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
+
     @Override
     public User createUser(UserDTO userDTO) throws DataNotFoundException {
         String email = userDTO.getEmail();
@@ -35,18 +44,33 @@ public class UserService implements IUserService{
                 .orElseThrow(() ->  new DataNotFoundException("Role is not found."));
         newUser.setRole(role);
         //Kiểm tra nếu có accountId thì ko yêu cầu mât khẩu
-        if(userDTO.getFacebookAccountId() == null && userDTO.getGoogleAccountId() == null){
+        if(userDTO.getFacebookAccountId() == null && userDTO.getGoogleAccountId() == null) {
             String password = userDTO.getPassword();
             //Se lam trong phan Spring Security
-//            String password = userDTO.getPassword();
-//            String encodedPassword = passwordEncoder.encode(password);
-            return userRepository.save(newUser);
+            String encodedPassword = passwordEncoder.encode(password);
+            newUser.setPassword(encodedPassword);
         }
-        return null;
+        return userRepository.save(newUser);
+
     }
 
     @Override
-    public String login(String email, String password) {
-        return "";
+    public String login(String email, String password) throws DataNotFoundException {
+        Optional<User> optionalUser =  userRepository.findByEmail(email);
+        if(optionalUser.isEmpty()){
+            throw new DataNotFoundException("Invalid enmail/password");
+        }
+        User existingUser = optionalUser.get();
+        if(existingUser.getFacebookAccountId() == null &&
+                existingUser.getGoogleAccountId() == null){
+            if(!passwordEncoder.matches(password, existingUser.getPassword())){
+                 throw new BadCredentialsException("Invalid email or password");
+            }
+        }
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(email, password);
+        authenticationManager.authenticate(authenticationToken);
+        return jwtTokenUtil.generateToken(existingUser);
     }
+
 }
---
+++ shop/backend/src/main/java/com/g5/techdevices/techstore/components/JwtTokenUtil.java
@@ -0,0 +1,59 @@
+package com.g5.techdevices.techstore.components;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.*;
+import java.util.function.Function;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenUtil {
+    @Value("${jwt.expiration}")
+    private long expiration;
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+    public String generateToken(com.g5.techdevices.techstore.entity.users.User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", user.getEmail());
+        try {
+            String token = Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(user.getEmail())
+                    .setExpiration(new Date(System.currentTimeMillis() + expiration * 100L))
+                    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                    .compact();
+            return token;
+        } catch (Exception e) {
+            System.out.println("Cannot generate JWT token, error: " + e.getMessage());
+            return null;
+        }
+    }
+    private Key getSignInKey(){
+        byte[] bytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    public <T>  T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claim = this.extractAllClaims(token);
+        return claimsResolver.apply(claim);
+    }
+    private boolean isTokenExpired(String token) {
+         Date expirationDate = this.extractClaim(token, Claims::getExpiration);
+         return expirationDate.before(new Date());
+    }
+}
