ec2967cfc86c6958e02cd55ff87a77af07a4f36e
nanashi193 <dqhungg8357@gmail.com>
2025-10-10 10:46:13 +0700

    add read token and role

--- shop/frontend/src/app/services/auth.service.ts
+++ shop/frontend/src/app/services/auth.service.ts
@@ -26,4 +26,40 @@ export class AuthService {
         // return this.http.post<void>(`${this.baseUrl}/forgot-password`, { email });
         return of(void 0).pipe(delay(800)); // MOCK
     }
+
+    // --------
+    get token(): string | null {
+        return localStorage.getItem('access_token');
+    }
+
+    setToken(token: string) {
+        localStorage.setItem('access_token', token);
+    }
+
+    clearToken() {
+        localStorage.removeItem('access_token');
+    }
+
+    getCurrentUser(): any {
+        const token = this.token;
+        if (!token || !token.includes('.')) return null;
+        try {
+            const payload = JSON.parse(atob(token.split('.')[1]));
+            return payload; // { sub, email, roles, exp, ... }
+        } catch {
+            return null;
+        }
+    }
+
+    hasRole(role: string): boolean {
+        const user = this.getCurrentUser();
+        return user?.roles?.includes(role);
+    }
+
+    isLoggedIn(): boolean {
+        const user = this.getCurrentUser();
+        if (!user) return false;
+        if (user.exp && Date.now() / 1000 > user.exp) return false;
+        return true;
+    }
 }
