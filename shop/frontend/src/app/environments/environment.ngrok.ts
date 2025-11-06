8c7930bfb4422a654ce569a9d3b4b9eec2b5a358
Vo Phuong Quynh <hieunghiatom@gmail.com>
2025-11-06 18:50:03 +0700

    bug 1

--- shop/frontend/src/app/environments/environment.ngrok.ts
+++ shop/frontend/src/app/environments/environment.ngrok.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     // SỬA DÒNG NÀY
-    apiBaseUrl: 'https://runty-sonya-composedly.ngrok-free.dev ', //Thay o day = backend
+    apiBaseUrl: 'https://2c48f49d004c.ngrok-free.app/api/v1', //Thay o day = backend
     useMock: false
 };
