8efce9e3c8917c4ee1380d1dc8ffa7530b376d3c
nanashi193 <dqhungg8357@gmail.com>
2025-11-08 18:37:50 +0700

    (owner) fix delete products

--- shop/backend/src/main/java/com/g5/techdevices/techstore/repositories/cart/CartItemRepository.java
+++ shop/backend/src/main/java/com/g5/techdevices/techstore/repositories/cart/CartItemRepository.java
@@ -4,10 +4,11 @@ import com.g5.techdevices.techstore.entity.Cart.Cart;
 import com.g5.techdevices.techstore.entity.Cart.CartItem;
 import com.g5.techdevices.techstore.entity.products.ProductVariant;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
     // Tìm một món hàng dựa trên giỏ hàng VÀ phiên bản
     Optional<CartItem> findByCartAndVariant(Cart cart, ProductVariant variant);
+    List<CartItem> findByVariant_Product_Id(Long productId);
 }
