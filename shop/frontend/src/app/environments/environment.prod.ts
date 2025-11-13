efdf0d1152ff12bfaa5263efa4ab81ec83e0c2c5
thunder <trungdinh.dev@gmail.com>
2025-11-13 22:52:29 +0700

    public DONE

--- shop/frontend/src/app/environments/environment.prod.ts
+++ shop/frontend/src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     // SỬA DÒNG NÀY
-    apiBaseUrl: 'https://cases-berry-journalists-respected.trycloudflare.com/api/v1', //Thay o day = backend
+    apiBaseUrl: 'https://api.technox.space/api/v1', //Thay o day = backend
     useMock: false
 };
--- shop/frontend/src/app/environments/environment.ts
+++ shop/frontend/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:8080/api/v1',
+    apiBaseUrl: 'https://api.technox.space/api/v1',
     useMock: false //mock test UI
 };
